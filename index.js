6e13b5c4b3989c6b3407be1fbe51551e43d8008a
Lorenzo Kraft <omole911@gmail.com>
2021-12-22 14:06:01 +0400

    added all files

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+ window.onload = function () {
+    error();
+}
+
+function error() {
+    alert("Sorry this page and its content has been disabled. Please see the Admin for aditional details. ")
+};
+
+
